293b53e8298bb35c908baf5466b5b3f0670bf176
Hunk Chou <jnzhouhao@126.com>
2021-02-06 13:50:09 +0800

    单例模式示例

--- src/test/java/com/hunk/designpattern/singleton/SingletonTest.java
+++ src/test/java/com/hunk/designpattern/singleton/SingletonTest.java
@@ -2,6 +2,8 @@ package com.hunk.designpattern.singleton;
 
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+
 /**
  * 单例模式测试用例
  * @author zh0809
@@ -10,7 +12,7 @@ import org.junit.Test;
 public class SingletonTest {
 
     /**
-     * 懒汉式单例模式，没加锁
+     * 懒汉式单例模式，没加锁和加锁两种情况的并发问题
      */
     @Test
     public void lazySingletonTest01() {
@@ -19,6 +21,26 @@ public class SingletonTest {
         t1.start();
         t2.start();
         System.out.println("End");
+    }
+
+    /**
+     * 静态内部类单例模式，反射会出现线程安全问题
+     */
+    @Test
+    public void staticInnerClassSingletonTest() {
+        try {
+            Class<?> singletonClass = StaticInnerClassSingleton.class;
+            Constructor<?> c = singletonClass.getDeclaredConstructor(null);
+            // 强制访问
+            c.setAccessible(true);
+
+            Object instance1 = c.newInstance();
+            Object instance2 = c.newInstance();
+
+            System.out.println(instance1 == instance2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
